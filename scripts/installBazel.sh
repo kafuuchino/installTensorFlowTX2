fab8e03b2d7012dcd495e486dd41c2533e6a9a37
kafuuchino <soranomethods@yahoo.co.jp>
2018-02-06 15:38:13 +0800

    fix the problem of uncompatible version of java

--- scripts/installBazel.sh
+++ scripts/installBazel.sh
@@ -2,15 +2,16 @@
 # NVIDIA Jetson TX2
 # TensorFlow Installation
 # Install Bazel
-# Version 0.5.2 - v0.5.3 apparently has issues
+# Version 0.10.0 - this version has fixed the update of java version
 # We use the release distribution so that we don't have to build protobuf
 #
 INSTALL_DIR=$PWD
 cd $HOME
-wget --no-check-certificate https://github.com/bazelbuild/bazel/releases/download/0.5.2/bazel-0.5.2-dist.zip
-unzip bazel-0.5.2-dist.zip -d bazel-0.5.2-dist
-sudo chmod -R ug+rwx $HOME/bazel-0.5.2-dist
+wget --no-check-certificate https://github.com/bazelbuild/bazel/releases/download/0.10.0/bazel-0.10.0-dist.zip
+unzip bazel-0.10.0-dist.zip -d bazel-0.10.0-dist
+sudo chmod -R ug+rwx $HOME/bazel-0.10.0-dist
 # git clone https://github.com/bazelbuild/bazel.git
-cd bazel-0.5.2-dist
-./compile.sh 
+cd bazel-0.10.0-dist
+./compile.sh
 sudo cp output/bazel /usr/local/bin
+
